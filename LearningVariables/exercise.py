8a0c23dca70ebfd2025ac58af1257390823e8605
Tenneh Kanneh <tennehkannehofficial@gmail.com>
2024-11-12 10:26:58 -0700

    Learning Variables.

---
+++ LearningVariables/exercise.py
@@ -0,0 +1,9 @@
+
+# We check in a patient named John Smith
+patient_name = "John Smith"
+
+# He is 20 years old
+patient_age = 20
+
+# He is a new patient
+new_patient = True
---
+++ LearningVariables/variables.py
@@ -0,0 +1,15 @@
+
+# Varibles used to temporarly store data in a computers memorey
+age = 20
+
+price = 19.95
+
+# under_snake case for vairbles
+# could use single or double quotes for strings
+first_name = "Mosh"
+
+# Upper case for False and True
+is_online = False
+
+# print age
+print(age)
