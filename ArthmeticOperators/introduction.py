9d3c39052ee2ce975e47e7eba9d4df0826478ba3
Tenneh Kanneh <tennehkannehofficial@gmail.com>
2024-11-12 10:31:16 -0700

    first commit

---
+++ ArthmeticOperators/introduction.py
@@ -0,0 +1,20 @@
+# adddition
+pint(3 + 10)
+
+# subtraction
+print(3 - 10)
+
+# multiplication
+print(3 * 10)
+
+# exponent operator
+print(10 ** 3)
+
+# double division
+print(3 / 10)
+
+# integer division
+print(3 // 10)
+
+# module operator
+print(3 % 10)
