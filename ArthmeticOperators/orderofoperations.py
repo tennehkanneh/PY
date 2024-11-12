f66fcc97d2bc8e8eab3da339f607c96c17b1ac1a
Tenneh Kanneh <tennehkannehofficial@gmail.com>
2024-11-12 10:43:20 -0700

    Arthmetic operators.

---
+++ ArthmeticOperators/orderofoperations.py
@@ -0,0 +1,6 @@
+# PEMDAS
+# Parantheses
+# Exponents, Multiplication, Division, Modulo
+# Addition, Subtraction
+
+x = 10 + 3 * 2
---
+++ Comparisons/introduction.py
@@ -0,0 +1,16 @@
+# Boolean Expression
+
+# < less than
+# > greater than
+
+# <= less than or equal to
+# >= greater than or equal to
+
+# == equals
+# != not
+
+x = 3 > 2
+
+x = 3 != 2
+
+print(x)
