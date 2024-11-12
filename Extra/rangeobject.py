6b30a846f19bf953ac6b3d1922043fd4846979a1
Tenneh Kanneh <tennehkannehofficial@gmail.com>
2024-11-12 12:12:13 -0700

    Extra stuff range and tulips.

---
+++ Extra/rangeobject.py
@@ -0,0 +1,6 @@
+
+numbers = range(5, 10 , 2)
+
+# retruns number 0 - 5
+for number in range(5): 
+    print(number)
---
+++ Extra/watulips.py
@@ -0,0 +1,9 @@
+# tulips are like lists bu there inmutible
+numbers = (1, 2, 3, 3)
+
+# if you try re-assining you will get an error
+
+# count method returns amount of repeated value in tupile
+print(numbers.count(3))
+
+# magic methods are with underscores
