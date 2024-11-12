c09240c87e032b135f38517cbc7316a2fcef9645
Tenneh Kanneh <tennehkannehofficial@gmail.com>
2024-11-12 12:01:07 -0700

    learning about lists.

---
+++ Lists/introduction.py
@@ -0,0 +1,14 @@
+# Here is a list
+flowers = ["Amaryllis", "Azalea", "Peony", "Asper", "Oleader", "Iris"]
+
+# print flowers
+print(flowers)
+
+# print flower at 0
+print(flowers[0])
+
+# can use negatives -1 is last element
+print(flowers[-1])
+
+# can print a range of values
+print(flowers[0:3])
---
+++ Lists/listmethods.py
@@ -0,0 +1,15 @@
+# if you want to see an objects availible methods do name. and the methods should appear in vs code
+
+numbers = [0, 1, 2, 3, 4, 5]
+print(numbers)
+
+# insert 6 into the numbers list at index 6
+numbers.insert(6, 6)
+print(numbers)
+
+# check if a number exits in the lsit
+print(3 in numbers)
+
+
+# length of the list
+print(len(numbers))
