afe2ebfb3f8d740398349c6e16ea5275729356db
Tenneh Kanneh <tennehkannehofficial@gmail.com>
2024-11-12 12:06:41 -0700

    Learning loops in python.

---
+++ Loops/for.py
@@ -0,0 +1,14 @@
+numbers = [1, 2, 3, 4, 5]
+
+print(numbers)
+
+# for loop
+for item in numbers:
+    print(item)
+
+# with a while loop
+i = 0
+
+while i < len(numbers):
+    print(numbers[i])
+    i += 1
---
+++ Loops/while.py
@@ -0,0 +1,8 @@
+i = 1
+
+# 1_000 = 1,000 makes numbers more readible
+while i <= 10:
+    # prints out the * as many tiems as the value of 5
+    # Example: if i = 5 then ***** will be printed out
+    print(i * '*')
+    i = i + 1
